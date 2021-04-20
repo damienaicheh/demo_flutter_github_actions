1f98e1308d3fd007f254c22d7af22488f11cfa07
Damien AICHEH <damien.aicheh@gmail.com>
2021-04-20 20:21:00 +0200

    Initial commit.

---
+++ lib/main.dart
@@ -0,0 +1,142 @@
+import 'dart:async';
+
+import 'package:battery_plus/battery_plus.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: HomeView(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class HomeView extends StatefulWidget {
+  HomeView({
+    Key key,
+    this.title,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            ElevatedButton(
+              child: Text('Show battery'),
+              onPressed: () => Navigator.of(context).push(
+                MaterialPageRoute<BatteryView>(
+                  builder: (context) => BatteryView(),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class BatteryView extends StatefulWidget {
+  BatteryView({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _BatteryViewState createState() => _BatteryViewState();
+}
+
+class _BatteryViewState extends State<BatteryView> {
+  final Battery _battery = Battery();
+
+  BatteryState _batteryState;
+  StreamSubscription<BatteryState> _batteryStateSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _batteryStateSubscription =
+        _battery.onBatteryStateChanged.listen((BatteryState state) {
+      setState(() {
+        _batteryState = state;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Battery'),
+      ),
+      body: Center(
+        child: Text('$_batteryState'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.battery_unknown),
+        onPressed: () async {
+          final batteryLevel = await _battery.batteryLevel;
+          // ignore: unawaited_futures
+          showDialog<void>(
+            context: context,
+            builder: (_) => AlertDialog(
+              content: Text('Battery: $batteryLevel%'),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text('OK'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    if (_batteryStateSubscription != null) {
+      _batteryStateSubscription.cancel();
+    }
+  }
+}
